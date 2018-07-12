12e9bdf658f548adbe84062141ac9a378a7caae3
Elishka Flint <elishkaf@gmail.com>
2018-07-12 17:21:56 +0100

    Add and minus JQuery functionality working

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,28 @@
 $( document ).ready(function() {
   var thermostat = new Thermostat();
 
-  $("#plus").click(function() {
-    thermostat.up();
-  });
-
   $("#current_temp").text(thermostat.getCurrentTemperature());
   $("#usage").text(thermostat.currentEnergyUsage());
+
+  $("#plus").click(function() {
+    thermostat.up();
+    $("#current_temp").text(thermostat.getCurrentTemperature());
+    $("#usage").text(thermostat.currentEnergyUsage());
+  });
+
+  $("#minus").click(function() {
+    thermostat.down();
+    $("#current_temp").text(thermostat.getCurrentTemperature());
+    $("#usage").text(thermostat.currentEnergyUsage());
+  });
+
+
 
   // $("#minus").click(thermostat.down())
   // $("#reset").click(thermostat.reset())
   // $("#ps_on").click(thermostat.powerSaveOn())
   // $("#ps_off").click(thermostat.powerSaveOff())
+
+
+
 });
