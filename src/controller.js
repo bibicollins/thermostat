1957e43ad5534121b23e33556787ab6a3f069b37
bibi collins <bibicollins94@googlemail.com>
2018-07-13 16:02:50 +0100

    Adding a weather API and getting it to function

--- src/controller.js
+++ src/controller.js
@@ -18,14 +18,36 @@ $( document ).ready(function() {
   $("#reset").click(function() {
     thermostat.reset();
     $("#current_temp").text(thermostat.getCurrentTemperature());
+    $("#usage").text(thermostat.currentEnergyUsage());
   })
   $("#ps_off").click(function() {
     thermostat.powerSaveOff();
+    $("#usage").text(thermostat.currentEnergyUsage());
   })
   $("#ps_on").click(function() {
     thermostat.powerSaveOn();
+  $("#usage").text(thermostat.currentEnergyUsage());
   })
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=fa84effbbc8cefc039ffc35edfd61842&units=metric', function(data) {
+    console.log(data.main.temp);
+    $('#current-temperature').text(data.main.temp)
+  })
+})
 
+  // $(document).ready(function(){
+  // $.ajax({
+  //   url: "http://api.openweathermap.org/data/2.5/weather?APPID=YOUR_API_KEY&id=&units=imperial",
+  //   dataType: 'json',
+  //   success: function(data) {
+  //     alert("Temperature is: " + data.main.temp );
+  //   },
+  //   error: function() {
+  //     alert("error");
+  //   }
+  // });
+// });
 
 
   // $("#minus").click(thermostat.down())
